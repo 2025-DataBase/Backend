1a4204b916e6bb150a884f69f59592b587c8059b
AnemoneCoronaria <love0604ms@naver.com>
2025-12-07 19:23:53 +0900

    db 최종

--- Computer_2.sql
+++ Computer_2.sql
@@ -5,13 +5,11 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 
-
 -- -----------------------------------------------------
 -- Schema Computer_2
 -- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `Computer_2` DEFAULT CHARACTER SET utf8 ;
-USE `computer_2` ; -- (수정 12/04 원가형) 스키마명 Computer_2 -> computer2 소문자 사용
-
+USE `computer_2` ;
 
 
 -- -----------------------------------------------------
@@ -25,7 +23,6 @@ CREATE TABLE IF NOT EXISTS `Computer_2`.`Team` (
 ENGINE = InnoDB;
 
 
-
 -- -----------------------------------------------------
 -- Table `Computer_2`.`Demon`
 -- -----------------------------------------------------
@@ -33,22 +30,14 @@ CREATE TABLE IF NOT EXISTS `Computer_2`.`Demon` (
   `demon_id` BIGINT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(100) NOT NULL,
   
-  -- 노션 인덱스 수정 명칭에 맞게 원래의 rank->grade로 수정
   `grade` ENUM('SS', 'S', 'A', 'B', 'C') NOT NULL COMMENT '악마의 위험도. 초기값 C',
   `status` ENUM('ALIVE', 'DEAD') NOT NULL,
   `bounty` BIGINT NULL COMMENT '현상금. 초기값 null. 현상금 초깃값 팀원들과 상의',
   `civilian_kills` BIGINT NOT NULL COMMENT '민간인 킬 수',
   `civilian_injuries` BIGINT NOT NULL COMMENT '민간인 부상 입힌 수',
   PRIMARY KEY (`demon_id`),
-  
-  -- D. 정렬 및 등급 조회 (Ranking & Sorting)
-  -- 목적: S급, A급 등 악마를 등급별로 조회하거나, 등급순으로 정렬할 때 성능을 높입니다.
-  INDEX `idx_demon_grade` (`grade` ASC)) -- (수정 12/04 원가형) 인덱스를 create 테이블 안에 넣음 
+  INDEX `idx_demon_grade` (`grade` ASC)) 
 ENGINE = InnoDB;
-
--- ALTER TABLE `Computer_2`.`Demon` -- (삭제 12/04 원가형)
--- ADD INDEX `idx_demon_grade`(`grade` ASC); -- (삭제 12/04 원가형)
-
 
 
 -- -----------------------------------------------------
@@ -76,16 +65,8 @@ CREATE TABLE IF NOT EXISTS `Computer_2`.`Hunter` (
     REFERENCES `Computer_2`.`Demon` (`demon_id`)
     ON DELETE SET NULL
     ON UPDATE CASCADE,
-    
-  -- A. 복합 인덱스 수정 및 human -> hunter 이름 수정
-  -- 목적: Human 테이블에서 "특정 팀"에 속해있으면서 "특정 상태(생존/사망 등)"인 사람을 찾을 때 사용합니다.
-  -- 효과: team_id로 먼저 거르고, 그 안에서 status로 한 번 더 빠르게 찾습니다.
-  INDEX `idx_hunter_team_status` (`team_id` ASC, `status` ASC)) -- (수정 12/04 원가형)
+  INDEX `idx_hunter_team_status` (`team_id` ASC, `status` ASC))
 ENGINE = InnoDB;
-
--- ALTER TABLE `Computer_2`.`Hunter` -- (삭제 12/04 원가형)
--- ADD INDEX `idx_hunter_team_status` (`team_id` ASC, `status` ASC); --(삭제 12/04 원가형)
-
 
 
 -- -----------------------------------------------------
@@ -98,17 +79,10 @@ CREATE TABLE IF NOT EXISTS `Computer_2`.`Mission` (
   `created_at` DATE NOT NULL COMMENT '생성 날짜',
   `due_date` DATE NOT NULL COMMENT '생성 마감',
   PRIMARY KEY (`mission_id`),
-  
-  -- C. 상태(State) 필터링 최적화
-  -- 목적: "현재 진행 중인 임무", "실패한 임무" 등 임무 상태별 조회를 빠르게 합니다.
   INDEX `idx_mission_status`(`state` ASC))
 ENGINE = InnoDB;
 
--- ALTER TABLE `Computer_2`.`Mission` -- (삭제 12/04 원가형)
--- ADD INDEX `idx_mission_status`(`state` ASC); -- (삭제 12/04 원가형)
 
-
--- pk가 없는 문제
 -- -----------------------------------------------------
 -- Table `Computer_2`.`Mission_assignment`
 -- -----------------------------------------------------
@@ -116,13 +90,8 @@ CREATE TABLE IF NOT EXISTS `Computer_2`.`Mission_assignment` (
   `team_id` BIGINT NOT NULL,
   `mission_id` BIGINT NOT NULL,
   
-  -- B. 외래키(FK) & 조인 성능 최적화
-  -- 목적: 특정 팀에게 할당된 임무 목록을 조회할 때 빠릅니다.
-  -- Mission이 아닌 Mission_assignment에 팀아이디에 대한 인덱스를 추가하였습니다.
-  -- 불필요한 Mission 테이블을 탐색하는거보다 이 테이블에서 거르는것이 더 효율적인 방법입니다.
   INDEX `team_id_idx` (`team_id` ASC) ,
   INDEX `mission_id_idx` (`mission_id` ASC) ,
-
   CONSTRAINT `assignment_team_id`
     FOREIGN KEY (`team_id`)
     REFERENCES `Computer_2`.`Team` (`team_id`)
@@ -136,8 +105,6 @@ CREATE TABLE IF NOT EXISTS `Computer_2`.`Mission_assignment` (
 ENGINE = InnoDB;
 
 
-
--- misson id에 외래키 제약 조건이 없음
 -- -----------------------------------------------------
 -- Table `Computer_2`.`Battle`
 -- -----------------------------------------------------
@@ -145,15 +112,11 @@ CREATE TABLE IF NOT EXISTS `Computer_2`.`Battle` (
   `mission_id` BIGINT NOT NULL,
   `battle_seq` BIGINT NOT NULL,
   `demon_id` BIGINT NOT NULL,
-  
-  -- 착오로 잘못 칭한 'HUMAN_WIN'->'HUNTER_WIN'으로 수정하였습니다.
-  `outcome` ENUM('HUNTER_WIN', 'DEMON_WIN') NOT NULL,
+  `outcome` ENUM('HUNTER_WIN', 'DEMON_WIN') NOT NULL,    
   `location` VARCHAR(100) NOT NULL,
   `civilian_killed` BIGINT NOT NULL,
   `civilian_injured` BIGINT NOT NULL,
-  -- PRIMARY KEY (`mission_id`, `battle_seq`),
-  
-  -- Mission 테이블 참조 외래 키 추가
+   PRIMARY KEY (`mission_id`, `battle_seq`),		
   CONSTRAINT `fk_battle_mission_id`
     FOREIGN KEY (`mission_id`)
     REFERENCES `Computer_2`.`Mission` (`mission_id`)
@@ -163,17 +126,11 @@ CREATE TABLE IF NOT EXISTS `Computer_2`.`Battle` (
     CONSTRAINT `fk_battle_demon_id`
     FOREIGN KEY (`demon_id`)
     REFERENCES `Computer_2`.`Demon` (`demon_id`)
-    ON DELETE SET NULL
+    ON DELETE RESTRICT
     ON UPDATE CASCADE,
   
-  -- B. 외래키(FK) & 조인 성능 최적화
-  -- 목적: 특정 악마가 참여한 전투 기록을 조회할 때 빠릅니다.
   INDEX `idx_battle_demon` (`demon_id` ASC))
 ENGINE = InnoDB;
-
--- ALTER TABLE `Computer_2`.`Battle` -- (삭제 12/04 원가형)
--- ADD INDEX `idx_battle_demon` (`demon_id` ASC); -- (삭제 12/04 원가형) 
-
 
 
 -- -----------------------------------------------------
@@ -191,19 +148,6 @@ CREATE TABLE IF NOT EXISTS `Computer_2`.`Reward` (
   INDEX `mission_id_idx` (`mission_id` ASC) ,
   INDEX `battle_seq_idx` (`battle_seq` ASC) ,
   INDEX `team_id_idx` (`team_id` ASC) ,
-  
--- 복합키로 수정
---   CONSTRAINT `reward_mission_id`
---     FOREIGN KEY (`mission_id`)
---     REFERENCES `Computer_2`.`Battle` (`mission_id`)
---     ON DELETE CASCADE
---     ON UPDATE CASCADE,
---   CONSTRAINT `reward_battle_seq`
---     FOREIGN KEY (`battle_seq`)
---     REFERENCES `Computer_2`.`Battle` (`battle_seq`)
---     ON DELETE CASCADE
---     ON UPDATE CASCADE,
-
   CONSTRAINT `fk_Reward_Battle`
   FOREIGN KEY (`mission_id`, `battle_seq`)
   REFERENCES `Computer_2`.`Battle` (`mission_id`, `battle_seq`)
@@ -218,7 +162,6 @@ CREATE TABLE IF NOT EXISTS `Computer_2`.`Reward` (
 ENGINE = InnoDB;
 
 
-
 -- -----------------------------------------------------
 -- Table `Computer_2`.`Battle_Participaton`
 -- -----------------------------------------------------
@@ -228,35 +171,19 @@ CREATE TABLE IF NOT EXISTS `Computer_2`.`Battle_Participaton` (
   `hunter_id` BIGINT NOT NULL,
   `participated_status` ENUM('ALIVE', 'DEAD') NOT NULL COMMENT '전투 후 헌터의 상태',
   PRIMARY KEY (`mission_id`, `battle_seq`, `hunter_id`),
-  
   INDEX `battle_seq_idx` (`battle_seq` ASC) ,
   INDEX `hunter_id_idx` (`hunter_id` ASC) ,
-
-  -- 복합키로 수정
-  --  CONSTRAINT `participation_mission_id`
-  --     FOREIGN KEY (`mission_id`)
-  --     REFERENCES `Computer_2`.`Battle` (`mission_id`)
-  --     ON DELETE CASCADE
-  --     ON UPDATE NO ACTION,
-  --   CONSTRAINT `participation_battle_seq`
-  --     FOREIGN KEY (`battle_seq`)
-  --     REFERENCES `Computer_2`.`Battle` (`battle_seq`)
-  --     ON DELETE CASCADE
-  --     ON UPDATE NO ACTION,
-
   CONSTRAINT `fk_participation_battle`
     FOREIGN KEY (`mission_id`, `battle_seq`)
     REFERENCES `Computer_2`.`Battle` (`mission_id`, `battle_seq`)
     ON DELETE CASCADE
     ON UPDATE NO ACTION,
-  
   CONSTRAINT `participation_hunter_id`
     FOREIGN KEY (`hunter_id`)
     REFERENCES `Computer_2`.`Hunter` (`hunter_id`)
     ON DELETE CASCADE
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
-
 
 
 -- -----------------------------------------------------
@@ -271,15 +198,8 @@ CREATE TABLE IF NOT EXISTS `Computer_2`.`Account` (
     REFERENCES `Computer_2`.`Hunter` (`hunter_id`)
     ON DELETE CASCADE
     ON UPDATE NO ACTION,
-    
-  -- D. 정렬 및 등급 조회 (Ranking & Sorting)
-  -- 목적: 보유 금액이 많은 순(랭킹)으로 정렬하거나, 특정 금액 이상/이하의 계정을 찾을 때 빠릅니다.
-  INDEX `idx_account_balance`(`balance` ASC)) -- (수정 12/04 원가형)
+  INDEX `idx_account_balance`(`balance` ASC)) 
 ENGINE = InnoDB;
-
--- ALTER TABLE `Computer_2`.`Account` -- (삭제 12/04 원가형)
--- ADD INDEX `idx_account_balance`(`balance` ASC); -- (삭제 12/04 원가형)
-
 
 
 -- -----------------------------------------------------
@@ -288,20 +208,12 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `Computer_2`.`Reward_Distribution` (
   `reward_id` BIGINT NOT NULL COMMENT '복합키. {reward_id(Fk->Reward),\nhunter_id(FK->Hunter)}',
   `hunter_id` BIGINT NOT NULL COMMENT '복합키. {reward_id(Fk->Reward),\nhunter_id(FK->Hunter)}',
-  
-  -- 돈이 지급중인지, 지급 완료 되었는지 나타내는 state가 추가되었습니다. 
   `state` ENUM('IN_PROGRESS', 'SUCCESS') NOT NULL ,
   `distributer_amount` BIGINT NOT NULL,
   `distributed_at` DATE NOT NULL,
   PRIMARY KEY (`reward_id`, `hunter_id`),
-  
-  -- B. 외래키(FK) & 조인 성능 최적화
-  -- 목적: 특정 헌터가 청구한 현상금 내역을 조회할 때 빠릅니다.
   INDEX `hunter_id_idx` (`hunter_id` ASC),
-  
-  -- C. 상태(Status) 필터링 최적화
-  -- 목적: 정산/배분 데이터 중 "지급 대기중"이나 "지급 완료" 건을 빠르게 찾습니다
-  INDEX `idx_dist_status`(`state` ASC), -- (수정 12/04 원가형)
+  INDEX `idx_dist_status`(`state` ASC),
 
   CONSTRAINT `distribution_reward_id`
     FOREIGN KEY (`reward_id`)
@@ -315,15 +227,7 @@ CREATE TABLE IF NOT EXISTS `Computer_2`.`Reward_Distribution` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
--- ALTER TABLE `Computer_2`.`Reward_Distribution` -- (삭제 12/04 원가형)
--- ADD INDEX `idx_dist_status`(`state` ASC); -- (삭제 12/04 원가형)
 
-
-
--- (추가 12/05 원가형)
--- 프로시저 코드
--- 기능 : 상금 분배 + Account 업데이트
--- 목적 : 기존 파이썬에서 처리하던 방식을 프로시저로 구현함으로써 빠르게 처리. 파이썬에선 call로 호출
 DROP PROCEDURE IF EXISTS distribute_bounty;
 DELIMITER ;;
 CREATE DEFINER=`root`@`localhost` PROCEDURE `distribute_bounty`(
@@ -428,11 +332,6 @@ END ;;
 DELIMITER ;
 
 
-
--- (추가 12/05 원가형)
--- 트리거 코드
--- 기능 : 헌터 등록시 헌터팀 자동 분배 자동화
--- 목적 : 헌터 정보 INSERT시 내부에서 동작하는 자동화 기능 구현을 위함
 DROP TRIGGER IF EXISTS trigger_random_team;
 DELIMITER $$
 CREATE TRIGGER trigger_random_team
@@ -516,6 +415,4 @@ TRUNCATE TABLE `Computer_2`.`Mission`;
 TRUNCATE TABLE `Computer_2`.`Account`;
 TRUNCATE TABLE `Computer_2`.`Hunter`;
 TRUNCATE TABLE `Computer_2`.`Team`;
--- 악마 데이터까지 삭제
--- TRUNCATE TABLE `Computer_2`.`Demon`;
 SET FOREIGN_KEY_CHECKS = 1;
